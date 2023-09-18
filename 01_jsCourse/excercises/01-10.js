67fdc942f751cea8fef02bcc84c7a35b8dc69625
fany <alvarezpestefania@outlook.com>
2023-09-18 12:51:12 -0600

    until video 36

---
+++ 01_jsCourse/excercises/01-10.js
@@ -0,0 +1,66 @@
+/*--1  Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.*/
+
+export function numCaracteres2(word) {
+    // opcion validando si hay texto o no
+    word == "" ? console.warn('no hay cadena') : console.log(word.length);
+    console.log(word.length);
+}
+
+// convirtiendo a array con return implicito
+export const numCaracteres = (word) => console.log(word.length);
+
+// numCaracteres('fany  hey!')
+
+
+
+/*-2 Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".*/
+export function recortWord(word, long) {
+    word == ""
+        ? console.warn('no hay cadena')
+        : console.log(word.slice(0, long));
+}
+export const recortWord2 = (word, long) =>
+    word == ""
+        ? console.warn('no hay cadena')
+        : console.log(word.slice(0, long));
+
+// recortWord('fany', 2)
+
+
+
+/*--3 Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].*/
+export function returnArray(word) {
+    word == ""
+        ? console.warn('no hay cadena')
+        : console.log(word.split(' '));
+}
+// returnArray('hola como estas')
+
+
+
+/*-- 4 Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.*/
+export function repitWord(word, repit) {
+
+    if (repit === 0) return console.error("EL numero de veces no puede ser 0");
+    if (Math.sign(repit) === -1) return console.error("EL numero de veces no puede negativo");
+
+    // let contador = 1
+    // let newWord = word
+    // if (word == "") {
+    //     console.warn('no hay cadena')
+    // } else {
+    //     while (contador !== repit) {
+    //         contador++
+    //         newWord = newWord + " " + word
+    //     }
+    // }
+    // return console.log(newWord);
+
+    //usando metodo repeat
+    return console.log(word.repeat(number))
+}
+// repitWord('hola 1 ',3)
+
+
+
+
---
+++ 01_jsCourse/excercises/02-10.js
@@ -0,0 +1,55 @@
+/* 5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".*/
+export const wordInvert = (word) => word === ''
+    ? console.warn('No haz ingresado el texto')
+    : console.log(word.split('').reverse().join(''));
+
+// wordInvert('Hola Mundo')
+
+
+
+/* 6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.*/
+export const wordRepit = (text, word) => {
+    if (!text) return console.warn('No haz ingresado el texto')
+    if (word === '') return console.warn('No haz ingresado la palabra a buscar')
+    // return  console.log(text.match(/mundo/gi).length)
+    let contador = ""
+    text.split(' ').forEach(element => {
+        element === word ? contador++ : false;
+    });
+    return console.log(contador)
+}
+// wordRepit("hola mundo adios mundo", "mundo")
+
+
+
+/* 7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.*/
+export const wordPolindro = (word) => {
+    if (word === '') return console.warn('No haz ingresado el texto')
+    let wordRevers = word.toLowerCase().split(' ').join('').split('').reverse().join('')
+    let wordCompare = word.toLowerCase().split(' ').join('').split('').join('')
+
+    wordRevers === wordCompare
+        ? console.log(true)
+        : console.log(false)
+}
+
+// wordPolindro('A ti no bonita')
+
+/*8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.*/
+
+export const wordsReplace = (text, word) => {
+    let newArray = []
+    text.split(',').forEach(element => {
+        newArray.push(element.replaceAll(word, ''))
+    })
+    return console.log(newArray.join(''))
+}
+// wordsReplace("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz")
+
+
+const eliminarCaracter = (text, word) => {
+    !text || !word
+        ? console.log("La informacion del texto o la palabra esta vacio")
+        : console.log(text.replace(new RegExp(word, "ig"), ''))
+}
+// eliminarCaracter('xyz1, xyz2, xyz3, xyz4 y xyz5', "xyz")
